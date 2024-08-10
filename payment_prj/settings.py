6495c11433d1bfd26aa5731aa3943965a096231d
Meranhor <128626138+Meranhor@users.noreply.github.com>
2024-08-10 16:22:35 +0300

    updated hosts for *

--- payment_prj/settings.py
+++ payment_prj/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("IS_DEVELOPPEMENT", True)
 
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "*")]
-ALLOWED_HOSTS = ["cpirolley.up.railway.app"]
+# ALLOWED_HOSTS = ["cpirolley.up.railway.app"]
 
 
 # Application definition
