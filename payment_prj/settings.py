99d10aaf8336b8dd41a5bc119ff4d584ce4b5c8c
Meranhor <128626138+Meranhor@users.noreply.github.com>
2024-08-10 16:50:32 +0300

    update hosts with getenv

--- payment_prj/settings.py
+++ payment_prj/settings.py
@@ -17,14 +17,16 @@ print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("IS_DEVELOPPEMENT", True)
 print(DEBUG)
+
 # ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "*")]
-ALLOWED_HOSTS = ["cpirolley.up.railway.app"]
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "*")]
+# ALLOWED_HOSTS = ["cpirolley.up.railway.app"]
 print(ALLOWED_HOSTS)
-# CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGINS", "*")]
-CSRF_TRUSTED_ORIGINS = [
-    'cpirolley.up.railway.app',
-]
+
+CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGINS", "*")]
+# CSRF_TRUSTED_ORIGINS = [
+#     'cpirolley.up.railway.app',
+# ]
 print(CSRF_TRUSTED_ORIGINS)
 # Application definition
 
